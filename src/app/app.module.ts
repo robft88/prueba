be21735fc2a406ba179390706104675219308913
Roberto Figueira <robft88@gmail.com>
2023-01-11 06:53:00 +0100

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,8 @@ import { SharedModule } from './shared/shared.module';
 import { NgChartsModule } from 'ng2-charts';
 // import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 // import { provideDatabase, getDatabase } from '@angular/fire/database';
 
 @NgModule({
@@ -29,8 +29,8 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
     HttpClientModule,
     NgChartsModule,
     SharedModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideDatabase(() => getDatabase())
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideDatabase(() => getDatabase())
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
     // provideDatabase(() => getDatabase())
   ],
